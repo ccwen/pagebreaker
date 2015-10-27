fd42be846a4625bcf6304a0be81ff69352aecb46
yapcheahshen <yapcheahshen@gmail.com>
2015-10-27 18:52:40 +0800

    first demo

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 59,
+ "title": "pagebreaker",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/ksanaforge/pagebreaker/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "react-dom",
+   "react-addons-update",
+   "react-addons-pure-render-mixin",
+   "ksana-search",
+   "ksana-jsonrom",
+   "ksana-database",
+   "ksana-analyzer"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js"
+ ],
+ "filesizes": [
+  502,
+  157,
+  672371,
+  544349,
+  277980,
+  804
+ ],
+ "filedates": [
+  "2015-10-27T10:01:18.823Z",
+  "2015-10-27T10:19:29.200Z",
+  "2015-10-27T09:36:14.497Z",
+  "2015-10-27T10:51:27.327Z",
+  "2015-10-27T09:32:56.304Z",
+  "2015-10-27T10:51:06.853Z"
+ ],
+ "date": "2015-10-27T10:51:28.251Z"
+})
---
+++ src/main.jsx
@@ -0,0 +1,95 @@
+var React=require("react");
+var ReactDOM=require("react-dom");
+var CodeMirror=require("ksana-codemirror").Component;
+var file1=require("./001-001");
+var Maincomponent = React.createClass({
+	getInitialState:function() {
+		return {data:file1,lastline:-1,pageid:"1.1a"}
+	}
+	,PBLINE:[]
+	,imageLoaded:function() {
+		var imageheight=ReactDOM.findDOMNode(this.refs.image).offsetHeight;
+		this.refs.cm.getCodeMirror().setSize(null,document.body.offsetHeight-imageheight);
+	}
+	,componentDidMount:function() {
+		this.cm=this.refs.cm.getCodeMirror();//codemirror instance
+		this.doc=this.cm.getDoc();
+		this.markAllTag();
+	}
+	,buildPBLINE:function() {
+		var marks=this.doc.getAllMarks();
+		this.PBLINE=[];
+		for (var i=0;i<marks.length;i++) {
+			var m=marks[i];
+			if (m.replacedWith.className=="pbmarker") {
+				var pos=m.find();
+				this.PBLINE.push([pos.from.line,m.replacedWith.innerHTML]);
+			}
+		}
+		this.PBLINE.sort(function(a,b){
+			return a[0]-b[0];
+		});
+	}
+	,getPageIdByLine:function(line) {
+		for (var i=1;i<this.PBLINE.length;i++) {
+			var pbline=this.PBLINE[i];
+			if (pbline[0]>line) {
+				return this.PBLINE[i-1][1];
+			}
+		}
+		return "1.1a";//default
+	}
+	,pageId2filename:function(pageid) {
+		var m=this.state.pageid.match(/(\d+)\.(\d+[abcd])/);
+		var vol="00"+m[1], pg="00"+m[2];
+		vol=vol.substr(vol.length-3);
+		pg=pg.substr(pg.length-4);
+		return "images/"+vol+"/"+vol+"-"+pg+".jpg";
+	}
+	,createMarker:function(tag) {
+		var element=document.createElement("SPAN");
+		if (tag.substr(0,2)=="pb") {
+			var id=tag.match(/"(.*?)"/);
+			element.className="pbmarker";
+			element.innerHTML=id[1];
+		}else {
+			element.className="tagmarker";	
+			element.innerHTML="<>";			
+		}
+		return element;
+	}
+	,markLineTag:function(i,rebuild) {
+		var line=this.doc.getLine(i);
+		var dirty=false;
+		line.replace(/<(.*?)>/g,function(m,m1,idx){
+			var element=this.createMarker(m1);
+			dirty=true;
+			this.doc.markText({line:i,ch:idx},{line:i,ch:idx+m.length},{clearOnEnter:true,replacedWith:element});
+		}.bind(this));
+		setTimeout(function(){
+			if (rebuild && dirty) this.buildPBLINE();
+		}.bind(this),100);//<pb id="1.2b"/>
+	}
+	,markAllTag:function() {
+		for (var i=0;i<this.doc.lineCount();i++){
+			this.markLineTag(i);
+		}
+		this.buildPBLINE();
+	}
+	,onCursorActivity:function(cm) {
+		var pos=cm.getCursor();
+		if (this.state.lastline>-1 && pos.line!==this.state.lastline) {
+			this.markLineTag(this.state.lastline,true);
+			var pageid=this.getPageIdByLine(pos.line);
+			this.setState({pageid:pageid});
+		}
+		this.setState({lastline:pos.line});
+	}
+  ,render: function() {
+    return <div>
+      <CodeMirror ref="cm" value={this.state.data} onCursorActivity={this.onCursorActivity}/>
+    	<img ref="image" src={this.pageId2filename()} onLoad={this.imageLoaded}/>
+    </div>;
+  }
+});
+module.exports=Maincomponent;
